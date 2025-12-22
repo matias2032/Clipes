8aa4e21d5ddbf311786a705458f45eacf11f9375
matias2032 <matiasmatavel1232@gmail.com>
2025-12-22 13:30:46 +0200

    Refactor database connection variables in conexao.php

--- api/conexao.php
+++ api/conexao.php
@@ -1,17 +1,17 @@
 <?php
 // A Vercel vai preencher getenv automaticamente com o que você salvou no painel
 $host = getenv('DB_HOST');
-$user = getenv('DB_USER');
-$pass = getenv('DB_PASS');
-$db   = getenv('DB_NAME');
+$usuario = getenv('DB_USER');
+$password = getenv('DB_PASS');
+$basededados   = getenv('DB_NAME');
 $port = getenv('DB_PORT');
 
 try {
     // Conectando usando a porta específica do Railway (44870)
-    $conn = new mysqli($host, $user, $pass, $db, $port);
+    $conexao = new mysqli($host, $usuario, $password, $basededados, $port);
     
-    if ($conn->connect_error) {
-        throw new Exception("Falha: " . $conn->connect_error);
+    if ($conexao->connect_error) {
+        throw new Exception("Falha: " . $conexao->connect_error);
     }
     // Sucesso! O site agora está conectado ao banco na nuvem.
 } catch (Exception $e) {
